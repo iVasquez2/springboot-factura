ead24a550dd28405ec6550e0ed8a2093034559b4
iVasquez <isra6314@outlook.com>
2024-03-05 12:57:30 -0600

    Estructura base del proyecto

---
+++ src/test/java/com/springboot/di/factura/springbootfactura/SpringbootFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.di.factura.springbootfactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
